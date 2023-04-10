e84d53b8344dc0630bc368af1ebed7b861cc43e8
Ed Schechter <schechtere@gmail.com>
2023-04-10 01:15:31 -0500

    Basic screens.  No database access yet

---
+++ src/pages/bookmark.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Bookmark = () => {
+  return (
+    <div>
+      <h1>Bookmark</h1>
+      <p>Welcome to the Bookmark page.</p>
+    </div>
+  );
+};
+
+export default Bookmark;
